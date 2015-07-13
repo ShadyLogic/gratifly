010c6facb0bd694245414418887f06a5b7180d39
ShadyLogic <JacobRogers42@gmail.com>
2015-07-12 21:17:54 -0700

    Implement treats system, change index page to reflect treat display, add methods for tracking treat interval and time left

--- app/controllers/treats.rb
+++ app/controllers/treats.rb
@@ -13,3 +13,14 @@ end
 delete '/treats/delete' do
   redirect "/"
 end
+
+post '/treats/complete' do
+	treat = Treat.find(params[:treat_id])
+	if treat.ready?
+		treat.collect
+		treat.reset_time
+		redirect "/"
+	else
+		"THIS TREAT IS NOT YET READY!"
+	end
+end
--- app/models/treat.rb
+++ app/models/treat.rb
@@ -1,3 +1,37 @@
 class Treat < ActiveRecord::Base
   belongs_to :user
+
+  # def set_time_start
+  # 	self.time_start = Time.now
+  # 	self.ready = false
+  # end
+
+  def make_ready
+  	self.ready = true
+  	self.save
+  end
+
+  def collect
+  	self.reset_time
+  	self.ready = false
+  	self.save
+  end
+
+  def reset_time
+  	self.time_start = Time.now
+  	self.save
+  end
+
+  def time_left
+  	self.interval_in_seconds - (Time.now - self.time_start)
+  end
+
+  def ready?
+  	elapsed_seconds = (Time.now - self.time_start).to_i
+  	time_left = self.interval_in_seconds - elapsed_seconds
+  	self.ready = true if time_left < 0
+  	self.save
+  	self.ready
+  end
+
 end
